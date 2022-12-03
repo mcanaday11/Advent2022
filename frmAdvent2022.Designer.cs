332532ccedd35fb640fa29052d495732d9f551e2
Matt <Matt@CANADAYRTX3080>
2022-12-03 01:02:34 -0600

    Add project files.

---
+++ frmAdvent2022.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Advent2022
+{
+    partial class frmAdvent2022
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(15, 15);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(903, 193);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(15, 217);
+            this.btnProcess.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(88, 27);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 255);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Result:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(14, 273);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(905, 221);
+            this.txtResult.TabIndex = 3;
+            // 
+            // frmAdvent2022
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtInput);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmAdvent2022";
+            this.Text = "Advent2022";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ frmAdvent2022.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Advent2022
+{
+    public partial class frmAdvent2022 : Form
+    {
+        public frmAdvent2022()
+        {
+            InitializeComponent();
+        }
+
+        public List<Elf>? Elves;
+        public List<Strategy>? Strategies;
+        public List<Rucksack> Rucksacks;
+        private string result = "";
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text;
+
+            //Day 1
+            //Elves = GetListofElves(input);
+            //ReportMaxCalories();
+
+            //Day 2
+            //Strategies = GetStrategyGuide(input);
+            //TournamentResults();
+
+            //Day 3
+            Rucksacks = GetRucksacks(input);
+            RucksackResults();
+        }
+
+        private void ReportMaxCalories() //Day 1
+        {
+            List<Elf> orderedElves = Elves.OrderByDescending(o => o.Calories).ToList();
+            
+            int bigWeight = 0;
+            int counter = 1;
+            foreach (Elf elve in orderedElves)
+            {
+
+                result += String.Format("[{0}] - {1}\r\n", elve.ElfNumber, elve.Calories);
+                bigWeight += elve.Calories;
+                counter++;
+                if (counter > 3)
+                {
+                    result += String.Format("Total Calories for Max 3: {0}\r\n", bigWeight);
+                    break;
+                }
+            }
+            txtResult.Text = result;
+        }
+
+        private List<Elf> GetListofElves(string input) //Day 1
+        {
+            List<Elf> list = new List<Elf>();
+
+            int calories = 0;
+            int elfNum = 1;
+            string[] lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    //End of elf, count calories, reset for next elf
+                    Elf elfo = new Elf();
+                    elfo.Calories = calories;
+                    elfo.ElfNumber = elfNum;
+                    list.Add(elfo);
+                    elfNum++;
+                    calories = 0;
+                } else
+                {
+                    calories += int.Parse(line); //Add calories
+                }
+            }
+            if (calories > 0) //Need to deal with the last elf
+            {
+                Elf elfo = new Elf();
+                elfo.Calories = calories;
+                elfo.ElfNumber = elfNum;
+                list.Add(elfo);
+            }
+            return list;
+        }
+
+        private List<Strategy> GetStrategyGuide(string input) //Day 2
+        {
+            List<Strategy> list = new List<Strategy>();
+            string[] lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            foreach (string line in lines)
+            {
+                Strategy strat = new Strategy();
+                strat.Opponent = Action(line[0]);
+                strat.RequiredResult = MatchResult(line[2]);
+                strat.Response = NeededResult(strat.RequiredResult, strat.Opponent);
+
+                list.Add(strat);
+            }
+            return list;
+        }
+
+        //private List<Rucksack> GetRucksacks(string input) //Day 3-1
+        //{
+        //    List<Rucksack> list = new List<Rucksack>();
+        //    string[] lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+        //    foreach (string line in lines)
+        //    {
+        //        Rucksack ruck = new Rucksack();
+        //        int mid = line.Length /2;
+
+        //        ruck.Left = line[..mid];
+        //        ruck.Right = line[mid++..];
+        //        list.Add(ruck);
+        //    }
+        //    return list;
+        //}
+        private List<Rucksack> GetRucksacks(string input) //Day 3-2
+        {
+            List<Rucksack> list = new List<Rucksack>();
+            string[] lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            int rowcount = 1;
+            List<string> tempList= new List<string>();
+
+            foreach (string line in lines)
+            {
+                tempList.Add(line);
+                if (tempList.Count() == 3)
+                {
+                    Rucksack ruck = new Rucksack();
+                    ruck.First = tempList[0];
+                    ruck.Second = tempList[1];
+                    ruck.Third = tempList[2];
+                    list.Add(ruck); 
+                    tempList.Clear();
+                }
+            }
+            return list;
+        }
+
+
+
+        private RPS Action(char input) //Day 2
+        {
+            if (input == 'A')
+            {
+                return RPS.Rock;
+            } else if (input == 'B')
+            {
+                return RPS.Paper;
+            } else if (input == 'C')
+            {
+                return RPS.Scissors;
+            }
+            return RPS.None;
+        }
+        private WLD MatchResult(char input) //Day 2
+        {
+
+            //return input switch
+            //{
+            //    'X' => 
+            //}
+
+            if (input == 'X')
+            {
+                return WLD.Loss;
+            }
+            else if (input == 'Y')
+            {
+                return WLD.Draw;
+            }
+            return WLD.Win;
+        }
+
+        private RPS NeededResult(WLD input, RPS opponent) //Day 2
+        {
+            switch (input)
+            {
+                case WLD.Loss: //Must lose
+                    if (opponent == RPS.Rock)
+                    {
+                        return RPS.Scissors;
+                    } else if(opponent == RPS.Paper)
+                    {
+                        return RPS.Rock;
+                    } 
+                    return RPS.Paper; 
+                case WLD.Draw: //Must Draw
+                    if (opponent == RPS.Rock)
+                    {
+                        return RPS.Rock;
+                    }
+                    else if (opponent == RPS.Paper)
+                    {
+                        return RPS.Paper;
+                    }
+                    return RPS.Scissors;
+                case WLD.Win: //Must Win
+                    if (opponent == RPS.Rock)
+                    {
+                        return RPS.Paper;
+                    }
+                    else if (opponent == RPS.Paper)
+                    {
+                        return RPS.Scissors;
+                    }
+                    return RPS.Rock;
+            }
+            return RPS.None;
+        }
+
+        private void TournamentResults() //Day 2
+        {
+            int totalScore = 0;
+
+            foreach (Strategy strat in Strategies)
+            {
+                result += String.Format("Needed Result: {0} [{1} {2} - {3}] --> Score {4}\r\n", strat.RequiredResult, strat.Opponent, strat.Response, strat.Result, strat.Score);
+                totalScore += strat.Score;
+            }
+            result += String.Format("Total Score: {0}\r\n", totalScore);
+            txtResult.Text = result;
+        }
+
+        private void RucksackResults() //Day 3
+        {
+            int totalValue = 0;
+
+            foreach (Rucksack ruck in Rucksacks)
+            {
+                result += String.Format("First: {0} Second: {1} Third: {2} Match: {3} Value: {4}\r\n", ruck.First, ruck.Second, ruck.Third, ruck.Match, ruck.Value);
+                totalValue += ruck.Value;
+            }
+            result += String.Format("Total Value: {0}\r\n", totalValue);
+            txtResult.Text = result;
+        }
+
+    }
+
+    public enum RPS //Day 2
+    {
+        None = 0,
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3
+    }
+
+    public enum WLD //Day 2
+    {
+        Win = 6,
+        Loss = 0,
+        Draw = 3
+    }
+
+    public class Strategy //Day 2
+    {
+        public RPS Opponent { get; set; }  
+        public RPS Response { get; set; }  
+
+        public WLD RequiredResult { get; set; } 
+        public WLD Result
+        {
+            get
+            {
+                return GetResult();
+            }
+        }
+
+        public int Score
+        {
+            get { return CalcScore(); }
+        }
+
+        private WLD GetResult()
+        {
+            if (Opponent == RPS.Rock)
+            {
+                switch (Response)
+                {
+                    case RPS.Rock:
+                        return WLD.Draw;
+                    case RPS.Paper:
+                        return WLD.Win;
+                    case RPS.Scissors:
+                        return WLD.Loss;
+                }
+            }
+            if (Opponent == RPS.Paper)
+            {
+                switch (Response)
+                {
+                    case RPS.Rock:
+                        return WLD.Loss;
+                    case RPS.Paper:
+                        return WLD.Draw;
+                    case RPS.Scissors:
+                        return WLD.Win;
+                }
+            }
+            if (Opponent == RPS.Scissors)
+            {
+                switch (Response)
+                {
+                    case RPS.Rock:
+                        return WLD.Win;
+                    case RPS.Paper:
+                        return WLD.Loss;
+                    case RPS.Scissors:
+                        return WLD.Draw;
+                }
+            }
+            return WLD.Loss; //Won't happen
+        }
+
+        private int CalcScore()
+        {
+            return (int)Result + (int)Response;
+        }
+    }
+
+    public class Elf //Day 1
+    {
+        public int ElfNumber { get; set; }
+        public int Calories { get; set; }
+    }
+
+    public class Rucksack //Day 3
+    {
+        public string? First { get; set; }
+        public string? Second { get; set; }
+        public string? Third { get; set; }
+        public char Match
+        {
+            get
+            {
+                var matches = First.Intersect(Second);
+                matches= matches.Intersect(Third);
+                return matches.FirstOrDefault();
+            }
+        }
+        public int Value { 
+            get
+            {
+                List<alphaValues> valueList = GetValueList();
+                return valueList.Find(x => x.theChar == Match).theValue;
+            }
+        }
+        private List<alphaValues> GetValueList()
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            List<alphaValues> values = new List<alphaValues>();
+            int value = 1;
+            foreach (char c in alphabet)
+            {
+                alphaValues av = new alphaValues
+                {
+                    theChar = c,
+                    theValue = value++
+                };
+                values.Add(av);
+            }
+            return values;  
+        }
+
+        public class alphaValues
+        {
+            public char theChar { get; set; }
+            public int theValue { get; set; }
+        }
+
+    }
+
+}
